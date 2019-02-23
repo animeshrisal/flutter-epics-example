2cd9108cc7307e4a3fa86100ebdc6b94681e9e5a
Animesh Risal <animeshrisal99@gmail.com>
2019-02-23 19:13:02 +0545

    get epic working

--- lib/epics/counterEpic.dart
+++ lib/epics/counterEpic.dart
@@ -1,14 +1,15 @@
 import 'package:redux_epics/redux_epics.dart';
 import 'package:flutter_epics_example/actions/CounterAction.dart';
 import '../models/AppState.dart';
+import 'package:flutter/foundation.dart';
 
 Stream<dynamic> counterEpic(
   Stream<dynamic> actions,
   EpicStore<AppState> store
 ) async* {
   await for(var action in actions){
-    if(action == IncreaseCounter){
-      yield IncreaseCounter();
+    if(action is IncreaseCounter){
+      new IncreaseCounter();
     }
   }
 }
